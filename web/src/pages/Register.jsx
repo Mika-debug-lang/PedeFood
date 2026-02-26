d5471a14e18b21fafb53899c48cf734c803a82e0
Mikael Dev <mikaelkauan2007@gmail.com>
2026-02-26 11:26:44 -0300

    Atualização backend produção

--- web/src/pages/Register.jsx
+++ web/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://delivery-backend.onrender.com";
 // 🔥 IMPORTANTE: use 127.0.0.1 para evitar conflito localhost/ipv6
 
 function Register() {
